93673007087c788057d4015399365f37ef33749c
413831 <alepleins3pp@gmail.com>
2020-08-23 19:04:29 -0300

    Este es un commit

---
+++ Ejemplo_1/cmake-build-debug/CMakeFiles/Ejemplo_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejemplo_1.dir/main.c.o"
+  "Ejemplo_1"
+  "Ejemplo_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ejemplo_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
