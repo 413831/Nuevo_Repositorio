c174072d99951e8eae8f382c6b4aee505e9eb295
413831 <alepleins3pp@gmail.com>
2020-09-08 17:21:10 -0300

    Ejemplo calculadora

---
+++ Mini_Calculadora/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroUno;
+    int numeroDos;
+    int resultado;
+    char operacion;
+
+    printf("Seleccione la operacion : suma (+), resta (-), multiplicacion (*), division (/): \n");
+    scanf("%c",&operacion);
+    printf("Ingrese los números a operar\n");
+    scanf("%d", &numeroUno);
+    scanf("%d", &numeroDos);
+    printf("%d %c %d = ", numeroUno, operacion, numeroDos);
+
+    switch(operacion)
+    {
+        case '+' :
+
+            resultado = numeroUno + numeroDos;
+            break;
+        case '-' :
+            resultado = numeroUno - numeroDos;
+            break;
+        case '*' :
+            resultado = numeroUno * numeroDos;
+            break;
+        case '/' :
+            resultado = numeroUno / numeroDos;
+            break;
+        default :
+            printf("Operacion invalida");
+    }
+
+    printf("%d", resultado);
+
+
+    return 0;
+}
